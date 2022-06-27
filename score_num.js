4cc6c4194bb6484dc2867ccbf22e3fc02a4bfe50
yoghurt <41856917+YoghurtGuy@users.noreply.github.com>
2022-06-27 15:35:51 +0800

    Update score_num.js

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const num = "40";return num;}getData().then((num) => {console.log(num);return num;});
+async function getData(){const num = "39_形势与政策(2)_通讯与网络（双语）";return num;}getData().then((num) => {console.log(num);return num;});
