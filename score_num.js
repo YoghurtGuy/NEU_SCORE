3d479dfc00ffc4528be870fb86599d6a0864ba61
UPDATE <leftongyu@gmail.com>
2022-06-27 09:32:22 +0000

    2022-06-27 09:32:22

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const course = "40_形势与政策(2)_智能医学3D打印技术（双语）_通讯与网络（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
+async function getData(){const course = "41_智能医学3D打印技术（双语）_形势与政策(2)_通讯与网络（双语）_心理学概论（在线式）";return course;}getData().then((course) => {console.log(course);return course;}); 
