1aa4de5bed138e949af326dba080de0c4d203f65
UPDATE <leftongyu@gmail.com>
2022-06-27 07:45:25 +0000

    2022-06-27 07:45:25

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const course = "39_形势与政策(2)_通讯与网络（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
+async function getData(){const course = "40_形势与政策(2)_智能医学3D打印技术（双语）_通讯与网络（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
