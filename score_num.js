f8e4b5a413d399eab019b72e366c8c6ef620ea99
yoghurt <41856917+YoghurtGuy@users.noreply.github.com>
2023-01-11 16:39:57 +0800

    Update score_num.js

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const course = "53_机器学习_数据库原理（双语）_软件工程（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
+async function getData(){const course = "52_机器学习_软件工程（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
