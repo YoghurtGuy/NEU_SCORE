5c52fc831746d931f192c2c0bd8ad31991087e8d
yoghurt <41856917+YoghurtGuy@users.noreply.github.com>
2023-02-25 11:06:51 +0800

    Update score_num.js

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const course = "55_毛泽东思想和中国特色社会主义理论体系概论_医学统计分析与临床决策支持_软件工程（双语）_机器学习_数据库原理（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
+async function getData(){const course = "54_毛泽东思想和中国特色社会主义理论体系概论_软件工程（双语）_机器学习_数据库原理（双语）";return course;}getData().then((course) => {console.log(course);return course;}); 
